037d7a2c02004879ab3974d5179366245cec8cbd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-02-22 11:49:44 +0600

    Payment js 🐬

--- src/Component/Dashboard/Payment.js
+++ src/Component/Dashboard/Payment.js
@@ -8,7 +8,7 @@ import {
   useElements,
 } from '@stripe/react-stripe-js';
 import { toast } from 'react-toastify';
-// Payment js
+// Payment JS
 const CheckoutForm = ({ order, clientSecret , setShow , refetch }) => {
   const date = useDate()
   const [loading , setLoading] = useState(false)
